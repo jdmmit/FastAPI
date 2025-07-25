a4bd2647f99c135684c95f4db8d78a2ff660be1f
jdmmit <juandavidmurillomejia0718@outlook.es>
2025-07-24 22:21:12 -0500

    24/07/2025 modificaciones

--- users.py
+++ users.py
@@ -7,12 +7,13 @@ app = FastAPI()
                 #   fastapi dev users.py
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
-users_list = [User(name="Brais", surname="Moure", url="https://mouredev.com", age=36), User(name="John", surname="Doe", url="https://johndoe.com", age=30), User(name="Pablo", surname="peladonerd", url="https://peladonerd.com", age=28)]
+users_list = [User(id=1, name="Brais", surname="Moure", url="https://mouredev.com", age=36), User(id=2, name="John", surname="Doe", url="https://johndoe.com", age=30), User(id=3, name="Pablo", surname="peladonerd", url="https://peladonerd.com", age=28)]
 
 @app.get("/usersjson")
 async def usersjson():
@@ -25,3 +26,20 @@ async def usersjson():
 @app.get("/users")
 async def users():
     return users_list
+
+# Path parameters
+@app.get("/user/{id}")
+async def get_user_by_path(id: int):
+    return search_user(id)
+
+# Query parameters
+@app.get("/user/")
+async def get_user_by_query(id: int):
+    return search_user(id)
+
+def search_user(id: int):
+    users = filter(lambda user: user.id == id, users_list)
+    try:
+        return list(users)[0]
+    except:
+        return {"error": "User not found"}
